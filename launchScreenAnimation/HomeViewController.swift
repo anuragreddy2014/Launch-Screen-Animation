c26cafe201f3fc3e4bb6b6b2ce51059681ac7c06
Anurag Reddy <anuragreddy@Anurags-MacBook-Pro.local>
2020-07-27 13:22:01 +0530

    first commit

---
+++ launchScreenAnimation/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  launchScreenAnimation
+//
+//  Created by Anurag Reddy on 26/07/20.
+//  Copyright © 2020 Anurag Reddy. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
